383ea37d5994dbc1b4b42bfd0770e10d720765ce
Andrei-David Nan <nandavid190@gmail.com>
2024-04-01 17:29:08 +0300

    v0.5 - config validate resource middleware;

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,4 @@
 export default {
-    SERVER_PORT: 1337,
-    DB_URI: "mongodb://localhost:27017/rest-api-tutorial",
+    SERVER_PORT: 8087,
+    DB_URI: "mongodb://localhost:27017/patients",
 }
--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,9 @@ import express from 'express';
 import config from 'config';
 import { connect } from './utils/connect';
 import { log } from './utils/logger';
+import { routes } from './routes';
 
-const port = config.get<number>('PORT');
+const port = config.get<number>('SERVER_PORT');
 
 const app = express();
 
@@ -11,4 +12,6 @@ app.listen(port, async  () => {
     log.info(`App is running on http://localhost:${port}`);
 
     await connect()
+
+    routes(app);
 })
