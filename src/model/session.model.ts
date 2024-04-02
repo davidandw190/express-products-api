d20b7618f742fa84249616a9626cd3c59ef84c79
Andrei-David Nan <nandavid190@gmail.com>
2024-04-02 19:52:39 +0300

    v1.0 - set up session.model.ts;

---
+++ src/model/session.model.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import { UserDocument } from './user.model';
+
+export interface SessionDocument extends mongoose.Document {
+    user: UserDocument['_id'];
+    isValid: boolean;
+    userAgent: string;
+
+    createdAt: Date;
+    updatedAt: Date;
+} 
+
+const sessionSchema = new mongoose.Schema(
+    {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+        isValid: { type: Boolean, default: true },
+        userAgent: { type: String },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const SessionModel = mongoose.model<SessionDocument>("Session", sessionSchema);
