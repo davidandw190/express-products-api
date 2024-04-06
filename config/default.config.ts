bc32ad8278bd276c5dfa9b3d10dcfbdaf5ea95c5
Andrei-David Nan <nandavid190@gmail.com>
2024-04-06 16:56:34 +0300

    v1.6 - signToken and verifyToken extensability improvements, docs additions;

--- config/default.config.ts
+++ config/default.config.ts
@@ -3,4 +3,6 @@ export default {
     SERVER_PORT: 8087,
     DB_URI: 'mongodb://localhost:27017/patients',
     SALT_ROUND_FACTOR: 12,
+    ACCESS_TOKEN_TTL: '15m',
+    REFRESH_TOKEN_TTL: '5d',
 }
--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -1,43 +1,59 @@
-import jwt from 'jsonwebtoken';
+import jwt, { SignOptions, VerifyOptions } from 'jsonwebtoken';
 import config from 'config';
 import { log } from './logger.utils';
 
+/**
+ * Signs a JWT token with the specified payload and private key.
+ * @param payload The payload to include in the JWT token.
+ * @param keyName The name of the configuration key containing the private key.
+ * @param options Optional signing options.
+ * @returns The signed JWT token.
+ */
 export function signToken(
-  object: Object,
+  payload: Object,
   keyName: 'accessTokenPrivateKey' | 'refreshTokenPrivateKey',
-  options?: jwt.SignOptions | undefined,
-) {
-  const signingKey = Buffer.from(
-    config.get<string>(keyName),
-    'base64',
-  ).toString('ascii');
-  return jwt.sign(object, signingKey, {
-    ...(options && options),
+  options?: jwt.SignOptions,
+): string {
+  const signingKey = Buffer.from(config.get<string>(keyName),'base64').toString('ascii');
+  
+  const signOptions: SignOptions = {
+    ...(options || {}),
     algorithm: 'RS256',
-  });
+  };
+  
+  return jwt.sign(payload, signingKey, signOptions);
 }
 
+/**
+ * Verifies a JWT token with the specified public key.
+ * @param token The JWT token to verify.
+ * @param keyName The name of the configuration key containing the public key.
+ * @returns An object indicating whether the token is valid, expired, and the decoded payload if valid.
+ */
 export function verifyToken(
   token: string,
   keyName: 'accessTokenPublicKey' | 'refreshTokenPublicKey',
-) {
-  const publicKey = Buffer.from(config.get<string>(keyName), 'base64').toString(
-    'ascii',
-  );
+): { valid: boolean; expired: boolean; decoded: any | null } {
+  const publicKey = Buffer.from(config.get<string>(keyName), 'base64').toString('ascii');
 
+  const verifyOptions: VerifyOptions = {
+    algorithms: ['RS256'],
+  };
+  
   try {
-    const decoded = jwt.verify(token, publicKey);
+    const decoded = jwt.verify(token, publicKey, verifyOptions);
 
     return {
       valid: true,
       expired: false,
       decoded,
     };
-  } catch (exception: any) {
-    log.error(exception);
+  } catch (error: any) {
+    log.error('Failed to verify JWT token:', error);
+    
     return {
       valid: false,
-      expired: exception.message.contains('jwt expired'),
+      expired: error.name === 'TokenExpiredError',
       decoded: null,
     };
   }
