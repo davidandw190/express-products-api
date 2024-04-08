fc7a94864a725cb9004ace9514d8361be8033011
Andrei-David Nan <nandavid190@gmail.com>
2024-04-08 18:49:20 +0300

    v1.14 - improve the way routin is set up for extensibility, add route not found and error handling middleware;

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,32 @@
 import { Express, Request, Response } from 'express';
-import { createUserHandler, updateUserHandler } from './resource/user.resource';
+import { createUserHandler, deleteUserHandler, updateUserHandler } from './resource/user.resource';
 import { createUserSchema, updateUserSchema } from './schema/user.schema';
 import { validate } from './middleware/validation.middleware';
 import { getUserSessionsHandler } from './resource/session.resource';
 
 export function routes(app: Express) {
-  app.get('/healthcheck', (_: Request, res: Response) => res.sendStatus(200));
+  // API Versioning
+  const apiVersion = '/api/v1';
 
-  app.post('/api/sessions', validate(createUserSchema), createUserHandler);
-  app.patch('/api/sessions', validate(updateUserSchema), updateUserHandler);
-  app.get('/api/sessions', getUserSessionsHandler)
+  // Health Check Route
+  app.get('/health', (_: Request, res: Response) => res.sendStatus(200));
+
+  // User Routes
+  app.post(`${apiVersion}/users`, validate(createUserSchema), createUserHandler);
+  app.patch(`${apiVersion}/users/:userId`, validate(updateUserSchema), updateUserHandler);
+  app.delete(`${apiVersion}/users/:userId`, deleteUserHandler);
+
+  // Session Routes
+  app.get(`${apiVersion}/users/:userId/sessions`, getUserSessionsHandler);
+
+  // Not Found Routes
+  app.use((_req, res) => {
+    res.status(404).send({ error: 'Route not found' });
+  });
+
+  // Error Handling Middleware
+  app.use((err: any, _req: Request, res: Response, _next: any) => {
+    console.error(err.stack);
+    res.status(500).send({ error: 'Internal Server Error' });
+  });
 }
